dff257652310a89288c0c97ed861184b123ed563
Nithish kumar <mnnithishkumar20@gmail.com>
2020-10-21 23:41:25 +0530

    Created User model 👾

---
+++ src/models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 7
+    }
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
