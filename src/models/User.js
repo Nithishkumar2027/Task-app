ea91f2b5842104489919ce422c451e9979adb531
Nithish kumar <mnnithishkumar20@gmail.com>
2020-10-25 23:34:12 +0530

    User signin module

--- src/models/User.js
+++ src/models/User.js
@@ -10,6 +10,7 @@ const userSchema = new mongoose.Schema({
     },
     email: {
         type: String,
+        unique: true,
         required: true,
         trim: true,
         lowercase: true,
@@ -36,6 +37,18 @@ userSchema.pre('save', async function (next) {
 
     next()
 })
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
+    if (!user) {
+        throw new Error('The email address that you\'ve entered doesn\'t match any account.')
+    }
+    const isMatch = await bcrypt.compare(password, user.password)
+    if (!isMatch) {
+        throw new Error('Unable to login')
+    }
+    return user
+}
 
 const User = mongoose.model('User', userSchema)
 
