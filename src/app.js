76105c35c75b60f392f374a8213dfcf4a04cb767
Nithish kumar <mnnithishkumar20@gmail.com>
2020-10-25 20:54:16 +0530

    Altering the user props

--- src/app.js
+++ src/app.js
@@ -26,13 +26,16 @@ app.get('/', (req, res) => {
     res.json({ msg: 'Task app 👋' })
 })
 
-const myFunction = async () => {
-    const password = 'helloww'
-    const hashedPassword = await bcrypt.hash(password, 8)
+// const myFunction = async () => {
+//     const password = 'helloww'
+//     const hashedPassword = await bcrypt.hash(password, 8)
 
-    console.log(password)
-    console.log(hashedPassword)
-}
+//     console.log(password)
+//     console.log(hashedPassword)
+
+//     const isMatch = await bcrypt.compare('password', hashedPassword)
+//     console.log(isMatch)
+// }
 
 myFunction()
 const port = process.env.PORT || 3000
