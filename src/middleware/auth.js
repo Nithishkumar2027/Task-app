f0d660eda77421449cc8742026f62ea9fcf57045
Nithish kumar <mnnithishkumar20@gmail.com>
2020-10-27 00:27:56 +0530

    Created Signin Auth middleware

---
+++ src/middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken')
+const { Error } = require('mongoose')
+const User = require('../models/User')
+
+const auth = async (req, res, next) => {
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '')
+        const decoded = jwt.verify(token, 'secret')
+        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
+        // console.log('~~~~~Decoded~~~~~~ \n', decoded)
+        if (!user) {
+            throw new Error()
+        }
+        req.user = user
+        next()
+
+    } catch (err) {
+        res.status(401).send({ error: 'Please authenticate' })
+    }
+}
+
+module.exports = auth
