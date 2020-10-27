ad11114b9d92a55692d9b1083e77e04e92c86112
Nithish kumar <mnnithishkumar20@gmail.com>
2020-10-27 23:59:31 +0530

    Added createdBy field

--- src/models/Task.js
+++ src/models/Task.js
@@ -10,6 +10,10 @@ const taskSchema = new mongoose.Schema({
     completed: {
         type: Boolean,
         default: false
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
     }
 })
 
