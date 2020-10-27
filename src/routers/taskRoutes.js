b87e280471753c49d1e63615d22149f7194bc954
Nithish kumar <mnnithishkumar20@gmail.com>
2020-10-28 00:19:58 +0530

    Created relationship with User model

--- src/routers/taskRoutes.js
+++ src/routers/taskRoutes.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const router = express.Router()
 
+const auth = require('../middleware/auth')
 const Task = require('../models/Task')
 
 // Creating Task
-router.post('/tasks', async (req, res) => {
-    const task = new Task(req.body)
+router.post('/tasks', auth, async (req, res) => {
+    const task = new Task({
+        ...req.body,
+        createdBy: req.user._id
+    })
 
     try {
         await task.save()
