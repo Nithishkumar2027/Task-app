2d64ebcdfcb25c7fb5743882653067cf8465e33c
Nithish kumar <mnnithishkumar20@gmail.com>
2020-10-29 00:31:43 +0530

    changed createdBy to author

--- src/routers/taskRoutes.js
+++ src/routers/taskRoutes.js
@@ -8,7 +8,7 @@ const Task = require('../models/Task')
 router.post('/tasks', auth, async (req, res) => {
     const task = new Task({
         ...req.body,
-        createdBy: req.user._id
+        author: req.user._id
     })
 
     try {
@@ -30,7 +30,7 @@ router.get('/tasks', auth, async (req, res) => {
 
     try {
         // Both approaches will work [by find() and populate()]
-        /* const tasks = await Task.find({ createdBy: req.user._id })
+        /* const tasks = await Task.find({ author: req.user._id })
            res.json(tasks) */
         await req.user.populate('tasks').execPopulate()
         res.json(req.user.tasks)
@@ -45,7 +45,7 @@ router.get('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id
 
     try {
-        const task = await Task.findOne({ _id, createdBy: req.user._id })
+        const task = await Task.findOne({ _id, author: req.user._id })
         console.log('Task: ', task)
         if (!task) {
             return res.status(404).json({
@@ -73,7 +73,7 @@ router.patch('/tasks/:id', auth, async (req, res) => {
     }
 
     try {
-        const task = await Task.findOne({ _id, createdBy: req.user._id })
+        const task = await Task.findOne({ _id, author: req.user._id })
         if (!task) {
             return res.status(404).send()
         }
@@ -93,7 +93,7 @@ router.patch('/tasks/:id', auth, async (req, res) => {
 router.delete('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id
     try {
-        const task = await Task.findOneAndDelete({ _id, createdBy: req.user._id })
+        const task = await Task.findOneAndDelete({ _id, author: req.user._id })
         if (!task) {
             return res.status(400).send()
         }
