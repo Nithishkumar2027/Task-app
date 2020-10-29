ff747576085cfebbc4e16f87f6607c3f3564d414
Nithish kumar <mnnithishkumar20@gmail.com>
2020-10-29 22:57:18 +0530

    Removed Deprecation warnings

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,3 +3,4 @@ const mongoose = require('mongoose')
 // DB connection
 const mongoURL = 'mongodb://localhost:27017/task-api'
 mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.set('useCreateIndex', true)
